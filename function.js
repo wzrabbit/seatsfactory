45161a36a665d10dccb8ccb4acd83266a705e4cf
polariswiz <66211382+polariswiz@users.noreply.github.com>
2020-06-07 17:45:33 +0900

    불러오기 디버깅

--- function.js
+++ function.js
@@ -347,7 +347,7 @@ function loadSeats() {
       makeTableAuto(layout[k], layout[k+1]);
     } //제작 작업
     console.log(value);
-    cell[0].className[0] = value[0];
+    cell[0].className = value[0];
     cell[0].value = value[1]; console.log(value[0]);
     for (i = 2; i < value.length; i = i + 2) {
       cell[i/2].className = value[i]; console.log(value[i]);
